289c40de018c39eb444da239cbb1ebd22c682f91
saugch <maria.guasch.m@gmail.com>
2016-10-08 23:49:45 +0200

    Final corrections.

--- templates/calculations.py
+++ templates/calculations.py
@@ -1,19 +1,6 @@
 import requests
 import json
 
-def generate_dates_list(total_initial_date, total_final_date, delta):
-   list_dates = []
-   initial_date = total_initial_date
-   final_date = initial_date + timedelta(days=delta)
-   list_dates.append((str(initial_date), str(final_date)))
-   
-   while final_date < total_final_date:
-       initial_date += timedelta(days = 1)
-       final_date = initial_date + timedelta(days=delta)
-       list_dates.append((str(initial_date), str(final_date)))
-       
-   return list_dates
-
 def ask_skyscanner(originplace,destinationplace,outbounddate,inbounddate,country='ES',currency='EUR',locale='spa',adults=1,groupPricing=False):
     info = {'apiKey': 'prtl6749387986743898559646983194', 'country': country, 'currency': currency, 'locale': locale,\
             'originplace': originplace, 'destinationplace': destinationplace, 'outbounddate': outbounddate,\
@@ -29,7 +16,7 @@ def ask_skyscanner(originplace,destinationplace,outbounddate,inbounddate,country
     else:
         return None
 
-def ask_hotelscanner(entityid,checkindate,checkoutdate,budget,guests=1,rooms=1,market='ES',currency='EUR',locale='spa'):
+def ask_hotelscanner(entityid,checkindate,checkoutdate,budget,total_price,guests=1,rooms=1,market='ES',currency='EUR',locale='spa'):
     headers = {'Accept': 'application/json'}
     a = requests.get('http://partners.api.skyscanner.net/apiservices/hotels/autosuggest/v2/'+market+'/'+currency+'/'+locale+'/'+entityid[0:3]+'?apikey=prtl6749387986743898559646983194', headers = headers)
     ucode = a.text
@@ -38,7 +25,7 @@ def ask_hotelscanner(entityid,checkindate,checkoutdate,budget,guests=1,rooms=1,m
     print 'We suppose you mean '+ a['results'][0]['display_name']+'.'
     
     entityid = a['results'][0]['individual_id']
-    
+        
     d = requests.get('http://partners.api.skyscanner.net/apiservices/hotels/liveprices/v2/'+market+'/'+currency+'/'+locale+'/'+entityid+'/'+checkindate+'/'+checkoutdate+'/'+str(guests)+'/'+str(rooms)+'?apiKey=prtl6749387986743898559646983194', headers = headers)
     g = requests.get('http://partners.api.skyscanner.net'+d.headers['Location']+'&sortColumn=rating&sortOrder=desc')
     
@@ -52,7 +39,7 @@ def ask_hotelscanner(entityid,checkindate,checkoutdate,budget,guests=1,rooms=1,m
     for i in xrange(len(d['hotels_prices'])):
         price = d['hotels_prices'][i]['agent_prices'][0]['price_total']
         if price < budget:
-            h.append((d['hotels'][i]['name'],price))
+            h.append((d['hotels'][i]['name'],total_price+price))
         if len(h) >= 3:
             return h
     return h
@@ -71,7 +58,7 @@ def life_costs(destinationplace,delta):
     else:
         return 100
 
-def ask_me(originplace, destinationplacelist, inoutbounddatelist, budget, delta,give_me_more=(0,0)):
+def ask_me(originplace, destinationplacelist, destinationcountryplacelist, inoutbounddatelist, budget, delta,travellers='1',currency='EUR',give_me_more=(0,0)):
     """ Given an origin and a destination, along with the length of the stay and the dates between which you
     would like to have it, it is able to return a combination of trip and hotel that fits the desired budget.
     
@@ -87,20 +74,53 @@ def ask_me(originplace, destinationplacelist, inoutbounddatelist, budget, delta,
     ------
     A hotel dictionary. You can acces to each date and to gmm (give_me_more variable).
     """
+    
+    querylist = destinationplacelist
+    destinationplacelist = []
+    j = 0
+    
+    d     = requests.get('http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/ES/GBP/en-GB/?query='+originplace+'&apiKey=prtl6749387986743898559646983194')
+    ucode = d.text
+    ucode = ucode.encode('ascii','ignore')      
+    d     = json.loads(ucode)
+    originplace = d['Places'][0]['CityId']
+    
+    print "We suppose you mean" + originplace +  d['Places'][0]['CountryName']
+    
+    for query in querylist:
+        d     = requests.get('http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/ES/GBP/en-GB/?query='+query+'&apiKey=prtl6749387986743898559646983194')
+        ucode = d.text
+        ucode = ucode.encode('ascii','ignore')      
+        d     = json.loads(ucode)
+        i     = 0
+        if type(d['Places']) == list:
+            l = len(d['Places'])
+        else:
+            l = 1
+        for i in xrange(l):
+            if destinationcountryplacelist[j] != d['Places'][i]['CountryName']:
+                pass
+            else:
+                destinationplacelist.append(d['Places'][i]['CityId'])
+            i += 1
+        j += 1
+    
     hlist = {}
     a, b  = give_me_more
     c, d  = a, b
+    i     = 0
     for destinationplace in destinationplacelist[a:]:
         for inoutbounddate in inoutbounddatelist[b:]:
             inbounddate, outbounddate = inoutbounddate
-            price  = ask_skyscanner(originplace,destinationplace,inbounddate,outbounddate)
+            price  = ask_skyscanner(originplace,destinationplace,inbounddate,outbounddate,adults=travellers,currency=currency)
             if price != None:
                 lcosts = life_costs(destinationplace, delta)
                 if price < (budget - lcosts):
                     ll         = [originplace,destinationplace,inbounddate,outbounddate]
-                    hotelprice = ask_hotelscanner(destinationplace,inbounddate,outbounddate,budget-lcosts-price)
+                    hotelprice = ask_hotelscanner(destinationplace,inbounddate,outbounddate,budget-lcosts-price,lcosts+price,guests=travellers,currency=currency)
                     if hotelprice != []:
-                        hlist[inbounddate] = hotelprice
+                        hlist['o'+str(i)] = [destinationplace,inbounddate,hotelprice]
+                        i += 1
                 if len(hlist) >= 3:
                     hlist['gmm'] = (c, d)
                     return hlist
@@ -108,9 +128,19 @@ def ask_me(originplace, destinationplacelist, inoutbounddatelist, budget, delta,
         b  = 0
         c += 1
     hlist['gmm'] = (c, d)
+    hlist = redefine_dictionary(hlist)
     return hlist
 
+def redefine_dictionary(d):
+    for i in xrange(len(d.keys())-1):
+        d['o'+str(i)][2]=sorted(d['o'+str(i)][2], key=lambda x: x[1])
+    return d
+        
+
 #print(ask_hotelscanner('bcn','2016-10-20','2016-10-21',1000))
+#print(ask_me('barcelona', ['valencia','london','new york'], ['Spain','Great Bretain','United Kindom'], llistadates, 1000, 4))
+
+
 
 from datetime import timedelta
 
@@ -134,4 +164,3 @@ def disp_coin(c):
         return 'GBP'
     else:
         return 'USD'
-    
--- templates/main.py
+++ templates/main.py
@@ -17,6 +17,7 @@ def calculate_result():
     
     from_city = request.form['from_city'].encode('ascii','ignore')
     to_city = [request.form['to_city'].encode('ascii','ignore')]
+    to_country = [request.form['to_country'].encode('ascii','ignore')]
     depart_date = datetime.strptime(request.form['depart_date'].encode('ascii','ignore'), "%Y-%m-%d").date()
     return_date = datetime.strptime(request.form['return_date'].encode('ascii','ignore').encode('ascii','ignore'), "%Y-%m-%d").date()
     num_days = int(request.form['num_days'].encode('ascii','ignore'))
@@ -24,15 +25,15 @@ def calculate_result():
     budget = int(request.form['budget'].encode('ascii','ignore'))
 
     chosen_currency = request.form.get('Currency')
+    coin = disp_coin(chosen_currency)
 
     inoutdates = generate_dates_list(depart_date, return_date, num_days)
 
-    print chosen_currency, type(chosen_currency)
     
     
-    
-    #d = ask_me(from_city, to_city, inoutdates, budget, num_days)
-    d =  {'gmm': (0, 2), 'o2': [u'VALE-sky', '2016-10-10', [(u'Vincci Lys', 656.66), (u'Sorolla Centro', 568.66), (u'Pio Xii Apartments Valencia', 485.65999999999997)]], 'o1': [u'VALE-sky', '2016-10-09', [(u'Vincci Lys', 709.99), (u'Sorolla Centro', 652.99), (u'Hotel Dimar', 642.99)]], 'o0': [u'VALE-sky', '2016-10-08', [(u'Sh Valencia Palace', 906.0899999999999), (u'Tryp Valencia Ocenic Hotel', 871.0899999999999), (u'Sercotel Sorolla Palace', 835.0899999999999)]]}
+    d = ask_me(from_city, to_city, to_country, inoutdates, budget, num_days, travellers_num, coin )
+    print d
+    #d =  {'gmm': (0, 2), 'o2': [u'VALE-sky', '2016-10-10', [(u'Vincci Lys', 656.66), (u'Sorolla Centro', 568.66), (u'Pio Xii Apartments Valencia', 485.65999999999997)]], 'o1': [u'VALE-sky', '2016-10-09', [(u'Vincci Lys', 709.99), (u'Sorolla Centro', 652.99), (u'Hotel Dimar', 642.99)]], 'o0': [u'VALE-sky', '2016-10-08', [(u'Sh Valencia Palace', 906.0899999999999), (u'Tryp Valencia Ocenic Hotel', 871.0899999999999), (u'Sercotel Sorolla Palace', 835.0899999999999)]]}
 
     #print from_city, to_city, inoutdates, budget, num_days, inoutdates
     #print 'res'#, d
@@ -46,24 +47,45 @@ def calculate_result():
     hotel00 = d['o0'][2][0][0]
     hotel01 = d['o0'][2][1][0]
     hotel02 = d['o0'][2][2][0]
-    preu_min0 = d['o0'][2][0][1]
+    preu_min0 = round(d['o0'][2][0][1],2)
 
-    ciutat1 = d['o1'][0]
-    dates1 = d['o1'][1]
-    hotel10 = d['o1'][2][0][0]
-    hotel11 = d['o1'][2][1][0]
-    hotel12 = d['o1'][2][2][0]
-    preu_min1 =  d['o1'][2][0][1]
-    
-    ciutat2 = d['o2'][0]
-    dates2 = d['o2'][1]
-    hotel20 = d['o2'][2][0][0]
-    hotel21 = d['o2'][2][1][0]
-    hotel22 = d['o2'][2][2][0]
-    preu_min2 =  d['o2'][2][0][1]
+    if len(d)>2:
+        ciutat1 = d['o1'][0]
+        dates1 = d['o1'][1]
+        hotel10 = d['o1'][2][0][0]
+        hotel11 = d['o1'][2][1][0]
+        hotel12 = d['o1'][2][2][0]
+        preu_min1 =  round(d['o1'][2][0][1],2)
+
+        if len(d)>3:
+            ciutat2 = d['o2'][0]
+            dates2 = d['o2'][1]
+            hotel20 = d['o2'][2][0][0]
+            hotel21 = d['o2'][2][1][0]
+            hotel22 = d['o2'][2][2][0]
+            preu_min2 =  round(d['o2'][2][0][1],2)
+        else:
+            ciutat2 = 'Blank'
+            dates2 = 'Blank'
+            hotel20 = 'Blank'
+            hotel21 = 'Blank'
+            hotel22 = 'Blank'
+            preu_min2 =  'Blank'
     
 
-    coin = disp_coin(chosen_currency)
+    else:
+        ciutat1, ciutat2 = 'Blank','Blank'
+        dates1, dates2 = 'Blank','Blank'
+        hotel10, hotel20 = 'Blank','Blank'
+        hotel11, hotel21 = 'Blank','Blank'
+        hotel12, hotel22 = 'Blank','Blank'
+        preu_min1, preu_min2 =  'Blank','Blank'
+
+
+
+
+    
+
     
     return render_template('result.html', ciutat0=ciutat0 , dates0=dates0, hotel00=hotel00, hotel01=hotel01, hotel02=hotel02 , preu0=preu_min0,ciutat1=ciutat1 , dates1=dates1, hotel10=hotel10, hotel11=hotel11, hotel12= hotel12 , preu1=preu_min1,ciutat2=ciutat2 , dates2=dates2, hotel20=hotel20, hotel21=hotel21, hotel22=hotel22 , preu2=preu_min2, moneda = coin)
     #return render_template('result.html')
